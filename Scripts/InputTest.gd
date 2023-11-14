da3f776757ea6ffe7d1d86684d9a0ea88f4ea999
wormymaple <65563085+wormymaple@users.noreply.github.com>
2023-11-14 10:57:26 -0700

    moving and grabbing

--- Scripts/InputTest.gd
+++ Scripts/InputTest.gd
@@ -1,31 +1,71 @@
 extends RigidBody2D
 
 @export var move_speed: float
+@export var jump_speed: float
+@export var arm_move_speed: float
+@export var grab_speed: float
+@export var arm_length: float
 
 @export var playerID: String
-@export var ring_path: NodePath
-var ring: Node2D
+@export var hand_path: NodePath
+@export var arm_path: NodePath
+@onready var hand = get_node(hand_path)
+@onready var arm = get_node(arm_path)
+var target_body: Node2D
+var grabbed_body: Node2D
 
 func _ready():
-	ring = get_node(ring_path)
+	hand.position = Vector2.UP * arm_length
+	arm.set_point_position(1, hand.position)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	var right_stick = Input.get_vector("right_left_" + playerID, "right_right_" + playerID, "right_up_" + playerID, "right_down_" + playerID)
+	var right_stick = Input.get_vector("right_left_" + playerID, "right_right_" + playerID, "right_up_" + playerID, "right_down_" + playerID).normalized()
 	if right_stick.length() != 0:
-		if !ring.visible:
-			ring.visible = true
-		ring.position = right_stick * 100
-	elif ring.visible:
-		ring.visible = false
+		hand.position += (right_stick * arm_length - hand.position) * arm_move_speed * delta
+		hand.position = hand.position.normalized() * arm_length
+		arm.set_point_position(1, hand.position)
+	
+	if grabbed_body != null:
+		var dir = hand.global_position - grabbed_body.global_position
+		grabbed_body.linear_velocity = dir * grab_speed
+	
+	if get_meta("grabbed") == true:
+		return
 	
 	var left_stick = Input.get_axis("left_left_" + playerID, "left_right_" + playerID)
 	if left_stick != 0:
 		linear_velocity.x = left_stick * move_speed
 	else:
 		linear_velocity.x = 0
+		
 
 func _input(event):
 	if event.is_action_pressed("left_trigger_" + playerID):
-		linear_velocity.y = -1000
+		linear_velocity.y = -jump_speed
+		
+	if event.is_action_pressed("right_trigger_" + playerID):
+		if grabbed_body == null and target_body != null:
+			grabbed_body = target_body
+			grabbed_body.gravity_scale = 0
+			grabbed_body.set_meta("grabbed", true)
+		elif grabbed_body != null:
+			grabbed_body.gravity_scale = 1
+			grabbed_body.set_meta("grabbed", false)
+			
+			grabbed_body = null
+		
+
+
+func _on_grab_area_body_entered(body):
+	if body == self:
+		return
 	
+	var grabbable = body.has_meta("grabbable")
+	if grabbable == true and body.get_meta("grabbed") != true:
+		target_body = body
+
+
+func _on_grab_area_body_exited(body):
+	if body == target_body:
+		target_body = null
