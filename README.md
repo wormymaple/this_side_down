0b5eb0073f1b9c1e7c1de31c1059d84450da1d0c
KJwacky <kjwacky13@gmail.com>
2023-11-14 11:00:52 -0700

    level 5 start

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # this_side_down
 Dev repo for This Side Down
+
+scooblis dooblis
+mooblis coo dis
+worshi porchi
+norschi borkshi
---
+++ Scenes/hor_moving_platform.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var direction = "left"
+@export var speed = 1
+
+func _ready():
+	_platform_move()
+
+
+
+func _process(delta):
+	if direction == "left":
+		position.x -= speed
+	elif direction == "right":
+		position.x += speed
+
+func _platform_move():
+	direction = "left"
+	await get_tree().create_timer(5).timeout
+	direction = "right"
+	await get_tree().create_timer(5).timeout
+	_platform_move()
