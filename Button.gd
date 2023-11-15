2ea61de1d3f1106519ab3acedb4e88e0cb8e2462
Lachly <60305985+Lachly@users.noreply.github.com>
2023-11-15 09:30:42 -0700

    added a level selecter

---
+++ Button.gd
@@ -0,0 +1,23 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	grab_focus()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_1.tscn")
+
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_2.tscn")
+
+
+func _on_button_3_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_3.tscn")
