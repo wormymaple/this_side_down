46560ba6a6584f881ba2272d5325cb45fe1dbdcf
wormymaple <65563085+wormymaple@users.noreply.github.com>
2023-11-15 10:05:18 -0700

    improve animations with sin

--- Scripts/InputTest.gd
+++ Scripts/InputTest.gd
@@ -63,7 +63,7 @@ func _physics_process(delta):
 			
 			var sample = wiggle_curve.sample(wiggle_current_time / wiggle_time)
 			legs.position = Vector2(sample * wiggle_intensity, legs.position.y)
-			head.position = Vector2(0, sample * head_bob)
+			head.position = Vector2(0, sin(wiggle_current_time / wiggle_time * PI) * head_bob)
 	else:
 		linear_velocity.x = 0
 		
