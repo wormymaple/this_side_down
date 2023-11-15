1d6f986c9c08a485da4f9b917fa1bf2dc741e141
Lachly <60305985+Lachly@users.noreply.github.com>
2023-11-15 09:40:14 -0700

    added more levels to play

--- Button.gd
+++ Button.gd
@@ -29,3 +29,7 @@ func _on_button_4_pressed():
 
 func _on_button_5_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Levels/level_5.tscn")
+
+
+func _on_button_6_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/level_6.tscn")
