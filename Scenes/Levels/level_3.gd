6bc0d4919baef0dab36d2feba24abbfd64aaebea
Oochie <142919558+Oochiiehehe3@users.noreply.github.com>
2023-11-30 10:01:50 -0700

    fixed music when you win level 3

--- Scenes/Levels/level_3.gd
+++ Scenes/Levels/level_3.gd
@@ -13,3 +13,9 @@ func _on_zone_body_body_entered(body):
 func win():
 	GlobalVariables.win_level(3)
 	get_node(landing_zone).play_particles()
+	await get_tree().create_timer(0.9).timeout
+	ThemeSongLoop.stop()
+	ThemeSongLoop.warehouse = false
+	ThemeSongLoop.intro2 = true
+	ThemeSongLoop.stream = load("res://Audio/AutomaticLabelMakerIntro.mp3")
+	ThemeSongLoop.play()
