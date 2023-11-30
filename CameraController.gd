a5bfa33a70ac75fd0c098d858d0d31d420b303e6
wormymaple <65563085+wormymaple@users.noreply.github.com>
2023-11-30 10:23:08 -0700

    set modulate on start for fade

--- CameraController.gd
+++ CameraController.gd
@@ -20,7 +20,7 @@ var fading
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	fade.modulate.a = 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
