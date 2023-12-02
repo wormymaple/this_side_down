ad01967fcae9c81bb71f1ba9d8764900e7bb3c73
Hiro Kahara <hirokahara@icloud.com>
2023-12-02 13:16:58 -0700

    Updated Level_1 + 2 and the Settings menu

--- settings_menu.gd
+++ settings_menu.gd
@@ -1,30 +1,28 @@
 extends Control
 
+#Video Button
+@onready var display_options = $MarginContainer2/MarginContainer/VBoxContainer/DisplayContainer/OptionButton
 
-#Video Settings
-@onready var display_options = $SettingTabs/Video/MarginContainer/VideoSettings/OptionButton
+#Audio Sliders
+@onready var master_vol_slider = $MarginContainer2/MarginContainer/VBoxContainer/MasterContainer/HBoxContainer/MasterVolSlider
+@onready var music_vol_Slider = $MarginContainer2/MarginContainer/VBoxContainer/MusicContainer/HBoxContainer2/MusicVolSlider
+@onready var other_vol_slider = $MarginContainer2/MarginContainer/VBoxContainer/EffectsContainer/HBoxContainer3/EffectsVolSlider
 
-#Audio Settings
-@onready var master_vol_slider = $SettingTabs/Audio/MarginContainer/AudioSettings/HBoxContainer/MasterVolSlider
-@onready var music_vol_Slider = $SettingTabs/Audio/MarginContainer/AudioSettings/HBoxContainer2/MusicVolSlider
-@onready var other_vol_slider = $SettingTabs/Audio/MarginContainer/AudioSettings/HBoxContainer3/OtherVolSlider
+const MUSIC_BUS = "Music"
+const OTHER_BUS = "Other"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta_):
+func _process(_delta_):
 	if Input.is_action_just_pressed("cancel") and visible:
 		hide()
 
+func _on_button_pressed():
+	hide()
 
 func _on_option_button_item_selected(index):
-	if index == 1:
+	if index == 1: # Fullscreen is 1
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
-	else:
+	else: # Windowed is 0
+		#print(index) 
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 
@@ -44,13 +42,3 @@ func _on_music_vol_slider_value_changed(value):
 func _on_other_vol_slider_value_changed(value):
 	var BusInt = AudioServer.get_bus_index(OTHER_BUS)
 	AudioServer.set_bus_volume_db(BusInt, value)
-
-const MUSIC_BUS = "Music"
-const OTHER_BUS = "Other"
-
-
-func _on_button_pressed():
-	$".".hide()
-
-
-
