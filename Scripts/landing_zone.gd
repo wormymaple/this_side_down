3033462f71ae9c2043b1a53f814823c32c1bcc33
Hiro Kahara <hirokahara@icloud.com>
2023-12-02 19:04:50 -0700

    Re-organized a little (Renamed the player scene)
    Renamed placeholder_player player because we use it as the player, and I re-organized the landing zone scene's structure to have the rigid body as the root because it just bugged me when the sprite showed up when you instantiated it.

--- Scripts/landing_zone.gd
+++ Scripts/landing_zone.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 @export var handler_script: NodePath
-@onready var handler = get_node(handler_script)
-@export var boxes_required: int
+@onready var handler = get_node(handler_script) # Bro why does this even get the script if its not actually the script just to do another variable that one @export could do
+@export var boxes_required: int = 1
 
 @export var particles: GPUParticles2D
 
@@ -12,13 +12,14 @@ func _process(_delta):
 	if len(colliding_bodies) >= boxes_required:
 		handler._on_zone_body_body_entered(colliding_bodies)
 
-func body_collide(body):
+func play_particles():
+	particles.emitting = true
+
+
+func _on_body_entered(body):
 	if body.is_in_group("Box"):
 		colliding_bodies.append(body)
 
-func body_exit(body):
+func _on_body_exited(body):
 	if body in colliding_bodies:
 		colliding_bodies.remove_at(colliding_bodies.find(body))
-		
-func play_particles():
-	particles.emitting = true
