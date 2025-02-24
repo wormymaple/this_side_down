b8059a4fa57a982c1da4742c58d55fd904eff554
Hiro Kahara <142524086+HiroKahara@users.noreply.github.com>
2025-02-24 08:59:15 -0700

    Renamed Art folders

--- Scenes/UI/MainmenuLevelScene.gd
+++ Scenes/UI/MainmenuLevelScene.gd
@@ -18,12 +18,12 @@ func _ready():
 	pass
 
 func _process(_delta):
-	if Input.is_action_just_pressed("left_up_p1"):
+	if Input.is_action_just_pressed("ui_up"):
 		move_arrow(-1)
-	if Input.is_action_just_pressed("left_down_p1"):
+	if Input.is_action_just_pressed("ui_down"):
 		move_arrow(1)
 	
-	if Input.is_action_just_pressed("confirm"):
+	if Input.is_action_just_pressed("ui_accept"):
 		call(menu_funcs[arrow_position])
 	
 	
