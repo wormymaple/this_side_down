e43496f8a10a076065c2a523f36338c3782dd55f
Hiro Kahara <142524086+HiroKahara@users.noreply.github.com>
2025-03-03 10:59:03 -0700

    Editing boat background

--- scenes/objects/spring.gd
+++ scenes/objects/spring.gd
@@ -15,30 +15,30 @@ func _ready():
 	elif theme == Themes.GREEN:
 		Sprite.texture = load("res://art/unused/spring.png")
 
-func _process(_delta): # Handle looking stretched ## When I get the spring split I shouldn't need to do this anymore
+func _process(delta): # Handle looking stretched ## When I get the spring split I shouldn't need to do this anymore
 	
 	
 	if current_state == State.IN: # Instead of 1, it's 0.082
 		if Sprite.scale.y > 0.041:
-			Sprite.scale.y -= .01
-			Sprite.position.y += 8.5 # 42.5 pixels down
+			Sprite.scale.y -= .2 * delta
+			Sprite.position.y += 160 * delta
 		else:
 			current_state = State.OUT
 	elif current_state == State.OUT:
 		if Sprite.scale.y < 0.164:
-			Sprite.scale.y += .01
-			Sprite.position.y -= 8.16667 # up 122.5 pixels
+			Sprite.scale.y += .2 * delta
+			Sprite.position.y -= 160 * delta # up 122.5 pixels
 		else: current_state = State.DONE
 	elif current_state == State.DONE:
 		if Sprite.scale.y > 0.082:
-			Sprite.scale.y -= .01
-			Sprite.position.y += 8 # down 80
+			Sprite.scale.y -= .2 * delta
+			Sprite.position.y += 160 * delta # down 80
 
 func _on_body_entered(body):
 	if body.is_in_group("Player") or body.is_in_group("Box"):
 		#print(abs(asin(rotation)))
 		if body.is_in_group("Player"):
-			body.unmovable = abs(asin(rotation)) > 0.1
+			body.unmovable = abs(asin(rotation)) > 0.1 # Sets if the player can control their direction midair based on the springs rotation
 		current_state = State.IN
 		
 		# var dir = Vector2(asin(rotation), -acos(rotation)).normalized() Old code, didn't work
--- scenes/other/background_boat.gd
+++ scenes/other/background_boat.gd
@@ -6,4 +6,4 @@ func _ready():
 	if bad_weather:
 		$Wayback/Ship.texture = load("res://art/area_3/ship_background_stormy.png")
 		for child in self.get_children():
-			child.modulate = Color(.5, .5, .5) # Darken everything else
+			child.modulate = Color(child.modulate.r + .5, child.modulate.r + .5, child.modulate.r + .5) # Darken everything else
