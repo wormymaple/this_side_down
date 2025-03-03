08dabc4768aaa293cf870e5bf1e684c500560a15
Hiro Kahara <142524086+HiroKahara@users.noreply.github.com>
2025-03-03 09:50:50 -0700

    Added back scripts to springs

--- scenes/other/background_boat.gd
+++ scenes/other/background_boat.gd
@@ -4,6 +4,6 @@ extends ParallaxBackground
 
 func _ready():
 	if bad_weather:
-		$Wayback/Ship.texture = load("res://Art/Area3/NewShipStormy.png")
+		$Wayback/Ship.texture = load("res://art/area_3/ship_background_stormy.png")
 		for child in self.get_children():
 			child.modulate = Color(.5, .5, .5) # Darken everything else
