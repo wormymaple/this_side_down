6aec3ccf2569db2f209b60d6a78ee9462b8ace8e
Hiro Kahara <142524086+HiroKahara@users.noreply.github.com>
2025-04-04 10:16:36 -0600

    Added new box sprites

--- scenes/objects/box.gd
+++ scenes/objects/box.gd
@@ -1,34 +1,52 @@
 extends RigidBody2D
 
-enum Themes {NORMAL, SHIP, UNDERWATER}
-@export var theme: Themes = Themes.NORMAL
+enum Themes {BOX, CRATE, SHIP, UNDERWATER}
+@export var theme: Themes = Themes.BOX
 @export var size: float = 1
 @onready var sprite = $BoxSprite
 @onready var collision = $Collision
 #@onready var arrow = $ArrowSprite
 
+# Not box sprites have a scale of 0.145
+# boxes should have a scale of 0.346
+
 func _ready():
 	if self.is_in_group("square"):
-		if theme == Themes.NORMAL:
-			sprite.texture = load("res://art/area_1/crate_square.png")
-		elif theme == Themes.SHIP:
-			sprite.texture = load("res://art/area_2/crate_square.png")
-		elif theme == Themes.UNDERWATER:
-			sprite.texture = load("res://art/area_4/crate_square.png")
+		match theme:
+			Themes.BOX:
+				sprite.texture = load("res://art/area_1/box_square.png")
+				sprite.scale = Vector2(0.346, 0.346)
+			Themes.CRATE:
+				sprite.texture = load("res://art/area_2/crate_square.png")
+			Themes.SHIP:
+				sprite.texture = load("res://art/area_3/crate_square.png")
+			Themes.UNDERWATER:
+				sprite.texture = load("res://art/area_4/crate_square.png")
 	elif self.is_in_group("circle"):
-		if theme == Themes.NORMAL:
-			sprite.texture = load("res://art/area_1/crate_circle.png")
-		elif theme == Themes.SHIP:
-			sprite.texture = load("res://art/area_2/crate_circle.png")
-		elif theme == Themes.UNDERWATER:
-			sprite.texture = load("res://art/area_4/crate_circle.png")
+		match theme:
+			Themes.BOX:
+				sprite.texture = load("res://art/area_1/box_square.png")
+				sprite.scale = Vector2(0.34, 0.34)
+			Themes.CRATE:
+				sprite.texture = load("res://art/area_2/crate_circle.png")
+			Themes.SHIP:
+				sprite.texture = load("res://art/area_3/crate_circle.png")
+			Themes.UNDERWATER:
+				sprite.texture = load("res://art/area_4/crate_circle.png")
 	else:
-		if theme == Themes.NORMAL:
-			sprite.texture = load("res://art/area_1/crate_triangle.png")
-		elif theme == Themes.SHIP:
-			sprite.texture = load("res://art/area_2/crate_triangle.png")
-		elif theme == Themes.UNDERWATER:
-			sprite.texture = load("res://art/area_4/crate_triangle.png")
+		match theme:
+			Themes.BOX:
+				sprite.texture = load("res://art/area_1/box_square.png")
+				sprite.scale = Vector2(0.37, 0.37)
+			Themes.CRATE:
+				sprite.texture = load("res://art/area_2/crate_triangle.png")
+				sprite.rotation_degrees = 179.4
+			Themes.SHIP:
+				sprite.texture = load("res://art/area_3/crate_triangle.png")
+				sprite.rotation_degrees = 179.4
+			Themes.UNDERWATER:
+				sprite.texture = load("res://art/area_4/crate_triangle.png")
+				sprite.rotation_degrees = 179.4
 		
 	sprite.scale *= Vector2(size, size) # Uses *= instead of just = because the starting sprite size is 0.145 
 	collision.scale *= Vector2(size, size)
