812a450661605a1e99b8ebc0a9857de3041414d3
Hiro Kahara <142524086+HiroKahara@users.noreply.github.com>
2025-05-01 08:52:11 -0600

    Concrete slabs now have randomly chosen textures

--- scenes/objects/concrete_slab.gd
+++ scenes/objects/concrete_slab.gd
@@ -1,10 +1,20 @@
 extends RigidBody2D
 
+var rng = RandomNumberGenerator.new()
 var can_play_sound = false
 
 func _ready() -> void:
 	$Sprite.scale *= scale
 	$CollisionPolygon2D.scale *= scale
+	
+	var sprite_version = rng.randi_range(1, 6)
+	if sprite_version == 1: 
+		pass # The texture will be uncracked, which is what the default is
+	elif sprite_version < 4: # 2 and 3
+		$Sprite.texture = load("res://art/general/concrete_slab_1.png")
+	else:# 4, 5, 6
+		$Sprite.texture = load("res://art/general/concrete_slab_3.png")
+	
 	#scale = Vector2i(1, 1) # This is done automaticallly by the game engine
 
 func _on_body_entered(body: Node):
